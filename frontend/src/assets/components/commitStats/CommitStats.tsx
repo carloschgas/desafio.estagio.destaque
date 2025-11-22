53d9a9042e27b586fe1c96427b4b08c76b198016
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-11-22 18:22:34 -0300

    refact: removed rectangle from rechart import

--- frontend/src/assets/components/commitStats/CommitStats.tsx
+++ frontend/src/assets/components/commitStats/CommitStats.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import styles from './commitStats.module.css'
 
 import type { CommitStatsProps } from '../../types/CommitStatsProps';
-import { BarChart, Bar, Rectangle, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 
 
