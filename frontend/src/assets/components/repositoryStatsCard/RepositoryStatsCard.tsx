fb93d90893c959e39e7ed6d1789237101849e9bd
carloschgas <139183141+carloschgas@users.noreply.github.com>
2025-11-22 12:07:09 -0300

    styles: added css and classes for repository cards

--- frontend/src/assets/components/repositoryStatsCard/RepositoryStatsCard.tsx
+++ frontend/src/assets/components/repositoryStatsCard/RepositoryStatsCard.tsx
@@ -7,9 +7,9 @@ import type { RepositoryCardProps } from "../../types/RepositoryCardProps"
 
 export default function RepositoryStatsCards({ type, count }: RepositoryCardProps) {
     return (
-        <div className="card">
-            <h3>{type}</h3>
-            <p>{count}</p>
+        <div className={styles.card}>
+            <h3 className={styles.type}>{type}: </h3>
+            <p className={styles.count}>{count}</p>
         </div>
     )
 }
